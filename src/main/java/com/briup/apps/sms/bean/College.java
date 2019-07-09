4f553888d8939e01a1fad31f87f00e44efffdfea
秦财武 <2641837080@qq.com>
2019-07-09 21:01:41 +0800

    [秦财武]终极版

---
+++ src/main/java/com/briup/apps/sms/bean/College.java
@@ -0,0 +1,33 @@
+package com.briup.apps.sms.bean;
+
+public class College {
+	private long id;
+	private String name;
+	private String description;
+	private long shool_id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public long getShool_id() {
+		return shool_id;
+	}
+	public void setShool_id(long shool_id) {
+		this.shool_id = shool_id;
+	}
+
+}
