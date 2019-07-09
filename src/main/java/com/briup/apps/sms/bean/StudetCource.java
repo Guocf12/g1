1d66824290e62a68d2c4d94c214f9fc5310863d9
邓小涛 <1444232600@qq.com>
2019-07-09 17:32:46 +0800

    【邓小涛】写的类

---
+++ src/main/java/com/briup/apps/sms/bean/StudetCource.java
@@ -0,0 +1,39 @@
+package com.briup.apps.sms.bean;
+
+public class StudetCource {
+	private long id;
+	private long user_id;
+	private long course_id;
+	private long xk_time;
+	private long grade;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+	public long getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(long course_id) {
+		this.course_id = course_id;
+	}
+	public long getXk_time() {
+		return xk_time;
+	}
+	public void setXk_time(long xk_time) {
+		this.xk_time = xk_time;
+	}
+	public long getGrade() {
+		return grade;
+	}
+	public void setGrade(long grade) {
+		this.grade = grade;
+	}
+}
