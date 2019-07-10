ac3f2c306769feb45a2961553ef7e6fac8392355
ASUS <2571790959@qq.com>
2019-07-10 17:28:31 +0800

    [田亚慧]部分代码提交

--- src/main/java/com/briup/apps/sms/service/impl/RoleServiceimpl.java
+++ src/main/java/com/briup/apps/sms/service/impl/RoleServiceimpl.java
@@ -1,5 +1,37 @@
 package com.briup.apps.sms.service.impl;
 
-public class RoleServiceimpl {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.sms.bean.Role;
+import com.briup.apps.sms.dao.RoleDao;
+import com.briup.apps.sms.service.RoleService;
+
+/**
+ * 校园逻辑逻辑处理的实现类
+ * */
+@Service
+public class RoleServiceimpl implements RoleService {
+	// 依赖注入，实例化SchoolDao并且赋值给schoolDao这个变量
+	@Resource
+	private RoleDao roleDao;
+
+	@Override
+	public List<Role> selectAll() {
+		// TODO Auto-generated method stub
+		return roleDao.selectAll();
+	}
+
+	@Override
+	public void saveOrUpdate(Role role) throws Exception {
+		if(role.getId()==null) {
+			roleDao.insert(role);
+		} else {
+			roleDao.update(role);
+		}
+		
+	}
 }
