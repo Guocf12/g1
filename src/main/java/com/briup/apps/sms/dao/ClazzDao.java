c7522d41a2f7d08528e8085a962539c87555d578
冯继虎 <528999721@qq.com>
2019-07-10 18:29:24 +0800

    [冯继虎] 部分代码修改

--- src/main/java/com/briup/apps/sms/dao/ClazzDao.java
+++ src/main/java/com/briup/apps/sms/dao/ClazzDao.java
@@ -1,5 +1,15 @@
 package com.briup.apps.sms.dao;
 
-public class ClazzDao {
+import java.util.List;
 
+import com.briup.apps.sms.bean.Clazz;
+
+public  interface ClazzDao {
+	//查询所有
+   List<Clazz>selectAll();
+   //插入
+   void insert(Clazz clazz);
+   //更新
+   void update(Clazz clazz);
+	
 }
