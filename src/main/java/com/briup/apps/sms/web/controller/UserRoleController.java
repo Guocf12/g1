249773ee6aa16c70aae4df3c057247b648173968
Administrator <Administrator@USERCHI-C51M8R7>
2019-07-10 19:24:56 +0800

    [张小芳]代码提交

--- src/main/java/com/briup/apps/sms/web/controller/UserRoleController.java
+++ src/main/java/com/briup/apps/sms/web/controller/UserRoleController.java
@@ -1,5 +1,24 @@
 package com.briup.apps.sms.web.controller;
 
-public class UserRoleController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.briup.apps.sms.bean.UserRole;
+import com.briup.apps.sms.service.UserRoleService;
+
+@RestController
+@RequestMapping("/userRole")
+public class UserRoleController {
+	@Autowired
+	private UserRoleService userRoleService;
+	//http://localhost:8080/clazz/selectAll
+@GetMapping("selectAll")
+public List<UserRole> selectAll(){
+	return userRoleService.selectAll();
+	
+}
 }
