a5ff7ff6128b79e5f2a50cc371395f5dcfe82df1
郭仓峰 <1095011528@qq.com.com>
2019-07-10 18:05:24 +0800

    【郭仓峰】查询各种

--- src/main/java/com/briup/apps/sms/service/impl/UserServiceimpl.java
+++ src/main/java/com/briup/apps/sms/service/impl/UserServiceimpl.java
@@ -1,9 +1,12 @@
 package com.briup.apps.sms.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.briup.apps.sms.bean.User;
 import com.briup.apps.sms.dao.UserDao;
 import com.briup.apps.sms.service.UserService;
 
@@ -16,6 +19,27 @@ public class UserServiceimpl implements UserService{
   //依赖注入，实例化UseDao并且赋值给UserDao这个变量
 	@Resource
 	private UserDao userDao;
+	
+
+	@Override
+	public List<User> selectAll() {
+		// TODO Auto-generated method stub
+		return userDao.selectAll();
+		
+	
+	}
+
+	public void saveOrUpdate(User user) throws Exception {
+	 
+
+		if(user.getId()==null) {
+			userDao.insert(user);
+		} else {
+			userDao.update(user);
+		}
+		
+		
+	}
 	
    
    
--- src/main/java/com/briup/apps/sms/web/controller/UserController.java
+++ src/main/java/com/briup/apps/sms/web/controller/UserController.java
@@ -1,7 +1,28 @@
 package com.briup.apps.sms.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.briup.apps.sms.bean.User;
+import com.briup.apps.sms.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+
 public class UserController {
 
 
+	@Autowired
+	private UserService userService;
+	//http://localhost:8080/school/selectAll
+@GetMapping("selectAll")
+public List<User> selectAll(){
+	return userService.selectAll();
+	
+}
 
 }
