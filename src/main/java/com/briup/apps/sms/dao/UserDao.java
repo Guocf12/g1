7e0671c13a5d6a07eced91a16210e4ba144dde11
郭仓峰 <1095011528@qq.com.com>
2019-07-10 12:52:01 +0800

    [郭仓峰]初始

--- src/main/java/com/briup/apps/sms/dao/UserDao.java
+++ src/main/java/com/briup/apps/sms/dao/UserDao.java
@@ -1,5 +1,15 @@
 package com.briup.apps.sms.dao;
 
-public class UserDao {
+import java.util.List;
 
+import com.briup.apps.sms.bean.User;
+
+public  interface UserDao {
+	//查询所有
+   List<User>selectAll();
+   //插入
+   void insert(User user);
+   //更新
+   void update(User user);
+	
 }
