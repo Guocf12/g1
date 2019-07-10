176901b59539ad3b30c0e194850bfe0b10e8c8cd
邓小涛 <1444232600@qq.com>
2019-07-10 09:45:19 +0800

    [邓小涛]修改小芳的

--- src/main/java/com/briup/apps/sms/bean/UserRole.java
+++ src/main/java/com/briup/apps/sms/bean/UserRole.java
@@ -4,4 +4,23 @@ public class UserRole {
 private Long id;
 private Long user_id;
 private Long role_id;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public Long getUser_id() {
+	return user_id;
+}
+public void setUser_id(Long user_id) {
+	this.user_id = user_id;
+}
+public Long getRole_id() {
+	return role_id;
+}
+public void setRole_id(Long role_id) {
+	this.role_id = role_id;
+}
+
 }
