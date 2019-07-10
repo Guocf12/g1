17700fb275c5ce6833ae18780ff4ce1b8cf954ee
蔡加强 <2275615154@qq.com>
2019-07-10 09:47:55 +0800

    [蔡加强]修改了SchoolDao

--- src/main/java/com/briup/apps/sms/dao/SchoolDao.java
+++ src/main/java/com/briup/apps/sms/dao/SchoolDao.java
@@ -1,43 +1,5 @@
 package com.briup.apps.sms.dao;
-/***
- * 
- * @author 蔡
- *
- */
+
 public class SchoolDao {
-	private Long id;
-	private String name;
-	private String description;
-	private Long tel;
-	private String address;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Long getTel() {
-		return tel;
-	}
-	public void setTel(Long tel) {
-		this.tel = tel;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+
 }
