b0290f175783fae3103f315199ee540237202334
张小芳 <1286762070@qq.com>
2019-07-11 15:43:30 +0800

    【张小芳】终极版

--- src/main/java/com/briup/apps/sms/dao/UserRoleDao.java
+++ src/main/java/com/briup/apps/sms/dao/UserRoleDao.java
@@ -6,14 +6,20 @@ import java.util.List;
 import com.briup.apps.sms.bean.UserRole;
 
 public interface UserRoleDao {
-	
-	List<UserRole> selectAll();
 	//查询所有
-	void insert(UserRole userRole);
+	List<UserRole> selectAll();
+	
+	
 	//插入
+	void insert(UserRole userRole);
+	
+		
+	 //更新
 	void update(UserRole userRole);
-    //更新
-	void deleteById(long id);
+   
+	
 	// 通过ID删除
+	void deleteById(long id);
+	
 	
 }
